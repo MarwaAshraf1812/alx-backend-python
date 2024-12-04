ac1afb5a95715948405c49092bc2c998d4e67b5a
MarwaAshraf1812 <marashraf090@gmail.com>
2024-12-04 23:56:45 +0200

    feat:  generator that streams rows from an SQL database

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,33 @@
+import mysql.connector
+
+def stream_users():
+    """
+    Streams user data from the ALX_prodev database.
+
+    Connects to the 'user_data' table in the ALX_prodev database and yields each row as a dictionary.
+    Closes the database connection and cursor after streaming the data.
+
+    Yields:
+        dict: A dictionary representing a row of user data.
+
+    Raises:
+        Exception: If an error occurs during database access or iteration.
+    """
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='root12',
+        database='ALX_prodev'
+    )
+    cursor = connection.cursor(dictionary=True)
+    
+
+    try:
+        cursor.execute("SELECT * FROM user_data")
+        for row in cursor:
+            yield row
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        connection.close()
