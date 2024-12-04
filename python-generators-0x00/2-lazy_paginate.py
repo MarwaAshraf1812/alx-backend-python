acfe02c2a5fb5c2afb0ce7e9749467411bb8ba1f
MarwaAshraf1812 <marashraf090@gmail.com>
2024-12-05 01:00:20 +0200

    feat: Lazy loading Paginated Data

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -3,6 +3,16 @@ seed = __import__('seed')
 
 
 def paginate_users(page_size, offset):
+    """
+    Retrieves a batch of user data from the ALX_prodev database.
+
+    Args:
+        page_size (int): The number of rows of user data to include in each batch.
+        offset (int): The row number to start the batch from.
+
+    Returns:
+        list of dict: A list of dictionaries, each representing a row of user data.
+    """
     connection = seed.connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
     cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
@@ -11,6 +21,15 @@ def paginate_users(page_size, offset):
     return rows
 
 def lazy_pagination(page_size: int):
+    """
+    A generator that yields batches of user data from the ALX_prodev database.
+
+    Args:
+        page_size (int): The number of rows of user data to include in each batch.
+
+    Yields:
+        list of dict: A list of dictionaries, each representing a row of user data.
+    """
     offset = 0
     while True:
         page = paginate_users(page_size, offset)
