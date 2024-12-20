9719b4be78d5635dd5fc7b586202ec34577b944a
MarwaAshraf1812 <marashraf090@gmail.com>
2024-12-20 21:46:55 +0200

    update: update urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,9 +3,18 @@ from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
-router.register(r'conversations', ConversationViewSet)
-router.register(r'messages', MessageViewSet)
+
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+'''
+This feature allows you to structure URLs in a hierarchy
+such as:
+/api/conversations/
+/api/conversations/<conversation_id>/messages/
+'''
+nested_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(nested_router.urls)),
 ]
