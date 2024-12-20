82b95a19a217648bc2dd2580c80faad4e249ebb1
MarwaAshraf1812 <marashraf090@gmail.com>
2024-12-20 20:55:41 +0200

    feat: create serializers to define the many to many relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -3,19 +3,38 @@ from .models import User, Conversation, Message
 
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['user_id', 'email', 'first_name',
+                  'last_name', 'full_name', 'phone_number', 'role']
+        read_only_fields = ['user_id', 'created_at']
 
 class MessageSerializer(serializers.ModelSerializer):
+    # Nested UserSerializer to include sender information
     sender = UserSerializer()
+    formatted_sent_at = serializers.SerializerMethodField()
     class Meta:
         model = Message
-        fields = '__all__'
+        fields = ['message_id', 'sender', 'message_body',
+                  'sent_at', 'formatted_sent_at']
+        read_only_fields = ['message_id','sent_at']
+
+    def get_formatted_sent_at(self, obj):
+        return obj.sent_at.strftime("%Y-%m-%d %H:%M:%S")
+    
+    def validate(self, data):
+        if 'message_body' not in data or data['message_body'] == '':
+            raise serializers.ValidationError("Message body cannot be empty")
+        return data
 
 class ConversationSerializer(serializers.ModelSerializer):
     participants = UserSerializer(many=True)
-    messages = MessageSerializer(many=True)
+    messages = MessageSerializer(many=True, read_only=True)
+    participants_count = serializers.SerializerMethodField()
     class Meta:
         model = Conversation
-        fields = '__all__'
+        fields = ['conversation_id', 'participants', 'messages', 'created_at']
+    
+    def get_participants(self, obj):
+      return obj.participants.count()
