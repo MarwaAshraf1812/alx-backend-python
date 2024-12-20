bfcc5562521c61603530ca2199c564ce9e5e25b8
MarwaAshraf1812 <marashraf090@gmail.com>
2024-12-20 22:40:23 +0200

    refactor: refactor messaging urls

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -2,12 +2,19 @@ from rest_framework import viewsets, status
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from django_filters import rest_framework as filters
+from .permissions import IsParticipant
 
 
 class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
+    permission_classes = [IsParticipant]
+
+    def get_queryset(self):
+        return Conversation.objects.filter(participants=self.request.user)
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
+    serializer_class = ConversationSerializer
+    permission_classes = [IsParticipant]
+
+    def get_queryset(self):
+        return Message.objects.filter(conversation__participants=self.request.user)
